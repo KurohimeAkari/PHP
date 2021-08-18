bbe4eb5d306422a41dac66a51072e36a387750b2
シスター † アンネ <yulin.iwasaki@gmail.com>
2021-08-19 08:34:49 +0900

    design update

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
 <title>PHP Research</title>
 </head>
 <body>
+	<h1>Module</h1>
 	<article>
 		<h2>ルビ作成 Module</h2>
 		<section>
 			<p><code><span>出力</span>：ruby('八宝菜','はっぽうさい');</code></p>
+			<p class='inst'><?php echo gitname('module/ruby.php');?></p>
 			<div>
 				<p>日本語ルビ：<? echo ruby('八宝菜','はっぽうさい'); ?></p>
 				<p>英語ルビ：<? echo ruby('素晴らしい','Excellent'); ?></p>
 			</div>
+			<p>ルビといえば難読漢字の上に付与する「ひらがな」ですが、いちいちとHTMLを書くのが面倒くさかったので制作しました</p>
 		</section>
 	</article>
 </body>
--- style/template.php
+++ style/template.php
@@ -6,3 +6,8 @@ function head() {
 	<link rel=preload href="//'.$_SERVER["HTTP_HOST"].'/style/style.css" as="style">
 	<link rel="stylesheet" href="//'.$_SERVER["HTTP_HOST"].'/style/style.css" type="text/css">';
 }
+
+function gitname($name){
+	$giturl ='https://github.com/KurohimeAkari/PHP/blob/master/';
+	echo '<a href="'.$giturl.$name.'" target="_blank">Install</a>';
+}
