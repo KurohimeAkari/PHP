07d83102f4330a4e2064c1e49540b8cec96003a1
シスター † アンネ <yulin.iwasaki@gmail.com>
2021-08-19 08:00:29 +0900

    Update ruby.php
    English ruby 判定付与 (使わないけどｗ)

--- module/ruby.php
+++ module/ruby.php
@@ -1,6 +1,12 @@
 <?
 function ruby($ruby,$rt){
 	$rpl = '('; $rpr = ')';
-	echo '<ruby><rp>'.$rpl.'</rp>'.$ruby.'<rt>'.$rt.'</rt><rp>'.$rpr.'</rp></ruby>';
+	// English
+	if (ctype_alpha($rt)){
+		$ctype = 'style="font-size:smaller;"';
+	} else {
+		$ctype = '';
+	}
+	echo '<ruby><rp>'.$rpl.'</rp>'.$ruby.'<rt '.$ctype.'>'.$rt.'</rt><rp>'.$rpr.'</rp></ruby>';
 }
 ?>
