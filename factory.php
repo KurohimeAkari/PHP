f68103c7701fa620eedc73a53ce7bb2560d131db
シスター † アンネ <yulin.iwasaki@gmail.com>
2021-08-22 10:05:45 +0900

    class化 テンプレ呼び出しと配列個々呼び出しができるように
    便利化

--- factory.php
+++ factory.php
@@ -22,10 +22,19 @@
 
 		<h2>csv array reader</h2>
 		<section>
-			<p><code><span>出力</span>：csv_array_reader();</code></p>
-			<p class='inst'><?php echo gitname('factory/counter.php');?></p>
+			<p><code><span>出力</span>：$csv[0]= new csv_Reader('array.csv');</code></p>
+			<p class='inst'><?php echo gitname('factory/csv_array_reader.php');?></p>
 			<div>
-				<p><? echo csv_array_reader('array.csv');?></p>
+				<p><?php
+				echo '<div>// csv_view() でテンプレ呼び出し</div>';
+				$csv[0]= new csv_Reader('array.csv'); echo $csv[0]->csv_view();
+				$csv[0]->csv_array_reader();
+				echo '<div>//csv_array_reader();で呼び出し [ factory.php ] で編集</div>';
+					foreach($csv[0]->csv_array as $key_v => $vals){
+						echo "<p>{$vals[0]}は{$vals[1]}と{$vals[2]}</p>";
+					}
+				echo '<div>// $csv[0]->csv_array[0][0]と記載すると一つだけ取り出せる。</div>';
+				echo $csv[0]->csv_array[0][0];?></p>
 			</div>
 			<p>Counter Program</p>
 		</section>
--- factory/csv_array_reader.php
+++ factory/csv_array_reader.php
@@ -1,21 +1,38 @@
 <?php
-	function csv_array_reader($filename){
-		if(file_exists( __DIR__ .'/'.$filename)){
-			$csv = file( __DIR__ .'/'.$filename);
+	class csv_Reader{
+	// このClassのみ
+	protected $filename;
+ 	public $csv_array;
+	function __construct($filename){
+		// 引数 $name を Class内で使える $this->name に
+		$this->filename = $filename;
+	}
+
+	public function csv_array_reader(){
+		//echo $this->filename;
+		if(file_exists( __DIR__ .'/'.$this->filename)){
+			$csv = file( __DIR__ .'/'.$this->filename);
 			$csv_body = array_splice($csv, 0);
 			// csv >> array [key][keys]
 			foreach($csv_body as $key => $row) {
 				$row_array = explode(',', $row);
 				foreach($row_array as $keys => $col){
 					$arr[$key][$keys] = $col;
+					$this->csv_array = $arr;
 				}
 			}
-			// 描写部分
-			foreach($arr as $key_v => $vals){
-				echo '<p>'.$vals[0].'は'.$vals[1].'の'.$vals[2].'です。</p>';
-			} 
-		} else {
+			return $this->csv_array;
+			} else {
 		echo 'Need to ' .$filename. ' file.';
 		}
 	}
+
+	public function csv_view(){
+		$this->csv_array_reader();
+		foreach($this->csv_array as $key_v => $vals){
+			echo "<p>{$vals[0]},{$vals[1]},{$vals[2]}</p>";
+		} 
+	}
+}
+
 ?>
